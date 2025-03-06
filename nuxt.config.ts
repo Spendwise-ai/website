b6f30ba4c3c51bdd490e6c21b1bf9604c05b1593
Mustafa Yousif <3urdparty@gmail.com>
2025-03-06 13:01:49 +0800

    Website

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import tailwindScrollbar from "tailwind-scrollbar";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: "/website/",
+  },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@vueuse/nuxt", "@nuxtjs/supabase", "@nuxt/ui", "@nuxt/icon"],
