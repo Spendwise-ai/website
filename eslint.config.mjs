bf5eb2e1f147e72cf7f6998d4dfaf0234f5218d2
Mustafa Yousif <3urdparty@gmail.com>
2025-03-15 10:22:57 +0800

    Changed ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,20 @@
-// @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import antfu from "@antfu/eslint-config";
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
 export default withNuxt(
-  // Your custom configs here
-)
+  antfu(
+    {
+      unocss: true,
+      formatters: true,
+    },
+    {
+      rules: {
+        // Allow trailing space in comments, for possible JSDoc formattings
+        "style/no-trailing-spaces": ["error", { ignoreComments: true }],
+        // Relaxes inline statements a bit
+        "style/max-statements-per-line": ["error", { max: 2 }],
+        "style/quotes": "off",
+      },
+    },
+  ),
+);
